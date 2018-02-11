42f6399ac326915160ae622cdf32e6b02816d341
jamie0 <jamie.woods@insanityradio.com>
2018-02-11 10:10:37 +0000

    Move migrations into independent statements

--- db/migrate/20180112204406_startup.rb
+++ db/migrate/20180112204406_startup.rb
@@ -1,23 +1,23 @@
 class Startup < ActiveRecord::Migration
 	def change
 
-		execute <<-SQL
+		begin
+			execute "alter table albums drop key `in_artist`;"
+		
+			execute "alter table albums drop key `external_id`;"
 
-			alter table albums drop key `in_artist`;
-			alter table albums drop key `external_id`;
+			execute "alter table artists drop key `external_id`;"
+			execute "alter table artists drop key in_artist;"
+			execute "alter table artists drop key index_artist;"
 
-			alter table artists drop key `external_id`;
-			alter table artists drop key in_artist;
-			alter table artists drop key index_artist;
+			execute "alter table migrate_cache drop key full;"
+			execute "alter table nerve_cache drop key `external_id`, drop key `n_uuid`, drop key `n_tr`, drop key `n_td`;"
 
-			alter table migrate_cache drop key full;
-			alter table nerve_cache drop key `external_id`, drop key `n_uuid`, drop key `n_tr`, drop key `n_td`;
+			execute "alter table tracks drop key in_artist, drop key in_album, drop key in_title, drop key index_title;"
 
-			alter table tracks drop key in_artist, drop key in_album, drop key in_title, drop key index_title;
-
-			alter table users drop key external_ref;
-
-		SQL
+			execute "alter table users drop key external_ref;"
+		rescue
+		end
 
 	end
 end
